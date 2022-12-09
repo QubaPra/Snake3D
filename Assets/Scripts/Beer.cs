fdad44f3a02977f0da3bee06b958166bb025edff
QubaPra <116064097+QubaPra@users.noreply.github.com>
2022-12-09 02:31:36 +0100

    version 1.0

--- Assets/Scripts/Beer.cs
+++ Assets/Scripts/Beer.cs
@@ -10,7 +10,7 @@ public class Beer : MonoBehaviour
 
     //current time
     private float timer = 0.0f;
-    private float spawnTime = 10;
+    private float spawnTime = 20;
 
     // Start is called before the first frame update
     void Start()
---
+++ Assets/Scripts/escMulti.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class escMulti : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Return()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+
+
+
+}
